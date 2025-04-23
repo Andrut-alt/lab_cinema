be842a630e7c240696155c5a33028861879dcddf
Andrut-alt <voloshyn.andriy2006@gmail.com>
2025-04-23 17:45:21 +0300

    Added posters to films

--- cinema/src/data/movies.js
+++ cinema/src/data/movies.js
@@ -1,27 +1,30 @@
+const posters = {
+    ...import.meta.glob('../assets/*.jpg', { eager: true, as: 'url' }),
+    ...import.meta.glob('../assets/*.png', { eager: true, as: 'url' })
+  };
 export const movies = [
-    {
-      id: 1,
-      title: "Дюна: Частина друга",
-      description: "Епічний науково-фантастичний фільм про боротьбу за пустельну планету Арракіс.",
-      genre: "Наукова фантастика",
-      poster: "https://i.imgur.com/4ZQZ3cC.jpg",
-      session: "2025-04-17 18:30"
-    },
-    {
-      id: 2,
-      title: "Барбі",
-      description: "Яскрава комедія про пригоди Барбі у реальному світі.",
-      genre: "Комедія",
-      poster: "https://i.imgur.com/2xjZ1l8.jpg",
-      session: "2025-04-17 20:00"
-    },
-    {
-      id: 3,
-      title: "Оппенгеймер",
-      description: "Біографічна драма про творця атомної бомби.",
-      genre: "Драма",
-      poster: "https://i.imgur.com/3zQZ3cC.jpg",
-      session: "2025-04-18 19:00"
-    }
-  ];
-  
+  {
+    id: 1,
+    title: "Дюна: Частина друга",
+    description: "Епічний науково-фантастичний фільм про боротьбу за пустельну планету Арракіс.",
+    genre: "Наукова фантастика",
+    poster: posters['../assets/dune.jpg'],
+    session: "2025-04-17 18:30"
+  },
+  {
+    id: 2,
+    title: "Барбі",
+    description: "Яскрава комедія про пригоди Барбі у реальному світі.",
+    genre: "Комедія",
+    poster: posters['../assets/barbie.jpg'],
+    session: "2025-04-17 20:00"
+  },
+  {
+    id: 3,
+    title: "Оппенгеймер",
+    description: "Біографічна драма про творця атомної бомби.",
+    genre: "Драма",
+    poster: posters['../assets/oppenheimer.png'],
+    session: "2025-04-18 19:00"
+  }
+];
