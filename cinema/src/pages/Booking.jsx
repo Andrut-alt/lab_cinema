ea8917670a70113d305d5355650a11cd5575d85a
Andrut-alt <voloshyn.andriy2006@gmail.com>
2025-04-23 20:28:02 +0300

    Upgrade Booking.jsx, connect form to booking

--- cinema/src/pages/Booking.jsx
+++ cinema/src/pages/Booking.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { movies } from "../data/movies";
 import CinemaHall from "../components/CinemaHall";
+import BookingForm from "../components/BookingForm";
 
 const Booking = () => {
   const { id } = useParams();
   const movie = movies.find((m) => m.id === parseInt(id));
+  const [selectedSeats, setSelectedSeats] = useState([]);
 
-  if (!movie) {
-    return <p>Фільм не знайдено</p>;
-  }
+  useEffect(() => {
+    const saved = localStorage.getItem(`booking-seats-${id}`);
+    if (saved) {
+      setSelectedSeats(JSON.parse(saved));
+    }
+  }, [id]);
+
+  if (!movie) return <p>Фільм не знайдено</p>;
 
   return (
     <div>
       <h2>Бронювання для: {movie.title}</h2>
       <p><b>Сеанс:</b> {movie.session}</p>
-      <CinemaHall movieId={movie.id} />
+      <CinemaHall movieId={movie.id} onSaveSeats={setSelectedSeats} />
+      {selectedSeats.length > 0 && (
+        <BookingForm movieId={movie.id} seats={selectedSeats} />
+      )}
     </div>
   );
 };
