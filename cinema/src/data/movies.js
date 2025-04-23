879b5f576009a648b406e716858f10bcc85267b2
Andrut-alt <voloshyn.andriy2006@gmail.com>
2025-04-23 17:17:48 +0300

    Create movie data

---
+++ cinema/src/data/movies.js
@@ -0,0 +1,27 @@
+export const movies = [
+    {
+      id: 1,
+      title: "Дюна: Частина друга",
+      description: "Епічний науково-фантастичний фільм про боротьбу за пустельну планету Арракіс.",
+      genre: "Наукова фантастика",
+      poster: "https://i.imgur.com/4ZQZ3cC.jpg",
+      session: "2025-04-17 18:30"
+    },
+    {
+      id: 2,
+      title: "Барбі",
+      description: "Яскрава комедія про пригоди Барбі у реальному світі.",
+      genre: "Комедія",
+      poster: "https://i.imgur.com/2xjZ1l8.jpg",
+      session: "2025-04-17 20:00"
+    },
+    {
+      id: 3,
+      title: "Оппенгеймер",
+      description: "Біографічна драма про творця атомної бомби.",
+      genre: "Драма",
+      poster: "https://i.imgur.com/3zQZ3cC.jpg",
+      session: "2025-04-18 19:00"
+    }
+  ];
+  
